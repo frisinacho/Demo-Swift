64ff3fdc98642e125246b75dca5663d0ed3b6f48
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-09 20:21:31 +0200

    Initial Commit

---
+++ Demo Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Demo Swift
+//
+//  Created by Ignacio Lasaosa Sáez on 9/9/15.
+//  Copyright (c) 2015 Ignacio Lasaosa Sáez. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+

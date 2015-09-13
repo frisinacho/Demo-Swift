941a3479ae343c5064e49f9804394d748e8a1ccd
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-13 19:22:41 +0200

    Cleaning up for loops

--- Demo Swift/main.swift
+++ Demo Swift/main.swift
@@ -8,12 +8,4 @@
 
 import Foundation
 
-var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
 
-switch meses[5] {
-    case "enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre": println("tiene 31 días")
-    case "febrero": println("tiene 28 días")
-    case "abril", "junio", "septiembre", "noviembre": println("tiene 30 días")
-    
-default: println("este caso no está contemplado")
-}

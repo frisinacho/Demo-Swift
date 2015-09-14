4392db059f04984c2c46595a3529938b50201150
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-14 13:24:24 +0200

    Cleaning up for While loop

--- Demo Swift/main.swift
+++ Demo Swift/main.swift
@@ -8,34 +8,4 @@
 
 import Foundation
 
-var i=0
 
-// For
-for i=1; i<4; i++ {
-    println(i)
-}
-
-// For in
-var array = [4, 866, 255, 986]
-
-for num in array{
-    println(num)
-}
-
-// Total for
-var Mayor = 0
-let series = [
-    "primos" : [2, 3, 5, 7],
-    "fibonacci" : [1, 1, 2, 3],
-    "potencia" : [2, 4, 8, 16]
-]
-
-for (elementoDiccionario, serieNumero) in series {
-    for numero in serieNumero {
-        if numero > Mayor {
-            Mayor = numero
-        }
-    }
-}
-
-println(Mayor)

9d74e177930d75fd6e9474964c2b8b95cf7011bc
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-15 14:31:06 +0200

    Cleaning up for multiple returns

--- Demo Swift/main.swift
+++ Demo Swift/main.swift
@@ -7,18 +7,3 @@
 //
 
 import Foundation
-
-func potencia (base : Int, exponente : Int) -> Int {
-    var total = 1;
-    var exp = exponente;
-    
-    while exp > 0 {
-        total = total * base
-        exp--
-    }
-    
-    return total
-}
-
-var resultado = potencia(7, 3)
-println("7^3 = \(resultado)")

25019f8b6dc7eefa4b2a1cd70f8b640e73ede8c6
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-16 11:57:42 +0200

    Cleaning up for recursion

--- Demo Swift/main.swift
+++ Demo Swift/main.swift
@@ -7,19 +7,3 @@
 //
 
 import Foundation
-
-func mediaTotal (numeros : Double...) -> ( Double, Double) {
-    var suma : Double = 2;
-    var cantidad : Double = 0;
-    
-    for num in numeros {
-        cantidad++
-        suma = suma + num
-    }
-    
-    return (suma, cantidad)
-}
-
-var resultado = mediaTotal(2, 4, 8, 10)
-var media = resultado.0/resultado.1
-println(media)
